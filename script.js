707f0522eef422f5c88c68614020047f44d7d69b
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2019-12-04 23:05:13 +0200

    Make prep time input numbers rather than text and change style accordingly

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if((array.filter(r => r.title === "firstRecipeDeletionMarker" || r.title === "Le
         ingredients: "2l veg or ham stock\n150g red lentils\n6 carrots, finely chopped\n2 medium leeks, sliced (300g)\nsmall handful chopped parsley, to serve", 
         directions: "Heat the stock in a large pan and add the lentils. Bring back to the boil and allow the lentils to soften for a few mins.\nAdd the carrots and leeks to the lentils and season (don't add salt if you use ham stock as it will make it too salty). Bring to the boil, the reduce the heat, cover and simmer for 45-60 mins until the lentils have broken down. Scatter over the parsley and serve with buttered bread, if you like.",
         servings: 4,
-        prepTime: "1h 10min"
+        prepTime: 70
     }
     array.push(obj);
 }
@@ -15,7 +15,7 @@ if ((array.filter(r => r.title === "secondRecipeDeletionMarker" || r.title === "
         title: "Broccoli soup with crispy croutons and goat's cheese",
         ingredients:"2 thick, crustless slices of bread, cubed\n1 tbsp olive oil\n900g broccoli\n50g butter\n1 large onion, finelychopped\ngenerous grating fresh nutmeg or ¼ tsp dried\n1l vegetable or chicken stock\n600ml full cream milk\n100g medium-soft goat's cheese, chopped (rind and all)",directions:"Preheat the oven to 200C/gas 6/fan 190C. Put the bread in a bowl and add the oil and a little salt. Mix well to coat the bread, then tip onto a baking tray. Bake for 10-12 minutes until crunchy and golden.\nMeanwhile, chop the broccoli stalks and florets, but keep them separate. Melt the butter in a pan, then add the onion, broccoli stalks and nutmeg and fry for 5 minutes until soft. Add the broccoli florets and stock, then the milk. Cover and simmer gently for 8 minutes until the broccoli is tender.\nTake out about four ladles of broccoli, then blend the rest in a food processor or with a hand blender, until smooth. Return the reserved broccoli to the soup and check for seasoning. (The soup will keep in the fridge for 2 days or you can cool and freeze it and keep the croutons in a plastic food bag.)\nTo serve, reheat if necessary and scatter with the croutons and goat’s cheese.",
         servings: 4,
-        prepTime: "40-50min"
+        prepTime: 40
     };
     array.push(obj);
 }
@@ -38,7 +38,7 @@ const listRecipes = () => {
 listRecipes();
 
 
-//If recipes are not empty showcase the first recipe and pre-filling its edit fields
+//If recipes are not empty showcase the first recipe and pre-fill its edit fields
 if ((JSON.parse(localStorage.getItem("therecipes"))).filter(r => r.title != "firstRecipeDeletionMarker" && r.title != "secondRecipeDeletionMarker").length != 0) {
     showRecipe(document.querySelector("#recipes-list").firstChild.innerHTML, true);
 }
@@ -56,7 +56,8 @@ function prefillEdit() {
     document.querySelector("#edit-ingredients").value = ing;
     document.querySelector("#edit-directions").value = dir;
     document.querySelector("#edit-servings").value = servings;
-    document.querySelector("#edit-prepTime").value = prepTime;  
+    document.querySelector("#edit-prepTime-hours").value = parseInt(prepTime / 60);
+    document.querySelector("#edit-prepTime-mins").value = prepTime % 60;  
 };
 
 //Save a recipe in localStorage from either create box or edit box (place)
@@ -74,7 +75,7 @@ function inputRecipe(place) {
                         "title": recipeName,
                         "ingredients": ing,
                         "directions": dir,
-                        "prepTime": document.querySelector(`#${place}-prepTime`).value,
+                        "prepTime": Number(document.querySelector(`#${place}-prepTime-hours`).value) * 60 + Number(document.querySelector(`#${place}-prepTime-mins`).value),
                         "servings": document.querySelector(`#${place}-servings`).value
                         }
         
@@ -93,7 +94,8 @@ function inputRecipe(place) {
         document.querySelector("#recipe-name").value = "";
         document.querySelector("#recipe-ingredients").value = "";
         document.querySelector("#recipe-directions").value = "";
-        document.querySelector("#recipe-prepTime").value = "";
+        document.querySelector("#recipe-prepTime-hours").value = "";
+        document.querySelector("#recipe-prepTime-mins").value = "";
         document.querySelector("#recipe-servings").value = "";
 
     } else {
@@ -112,13 +114,13 @@ function showRecipe(item, prefill) {
     let recipeDirectionsShow = obj.directions
                                         .split("\n")
                                         .map(item => `<li>${item}</li>`);
-    let recipePrepTime = obj.prepTime;
-    let recipeServings = obj.servings;
+    let recipePrepTimeHours = (parseInt(obj.prepTime / 60)) > 0 ? (parseInt(obj.prepTime / 60)) + "h" : "";
+    let recipePrepTimeMins = obj.prepTime % 60 > 0 ? obj.prepTime % 60 + "min" : "";
 
     document.querySelector("#showcase-name").innerHTML = item;
     document.querySelector("#showcase-ingredients").innerHTML = `<ul>${recipeIngredientsShow.reduce((item, sum) => item + sum)}</ul>`;
     document.querySelector("#showcase-directions").innerHTML = `<ol>${recipeDirectionsShow.reduce((item, sum) => item + sum)}</ol>`;
-    document.querySelector("#showcase-prepTime").innerHTML = recipePrepTime ? `<span><i class="fas fa-clock"></i><span> `+ `<span>${recipePrepTime}</span>` : "";
+    document.querySelector("#showcase-prepTime").innerHTML = recipePrepTimeHours || recipePrepTimeMins ? `<span><i class="fas fa-clock"></i><span> `+ `<span>${recipePrepTimeHours} ${recipePrepTimeMins}</span>` : "";
     document.querySelector("#showcase-servings").innerHTML = recipeServings ? `<span><i class="fas fa-utensils"></i></span> ` + `<span>${recipeServings}</span>` : "";
 
     if (prefill) {
@@ -140,7 +142,8 @@ function deleteRecipe() {
         document.querySelector("#showcase-name").innerHTML = "";
         document.querySelector("#showcase-ingredients").innerHTML = "";
         document.querySelector("#showcase-directions").innerHTML = "";
-        document.querySelector("#showcase-prepTime").innerHTML = "";
+        document.querySelector("#showcase-prepTime-hours").innerHTML = "";
+        document.querySelector("#showcase-prepTime-mins").innerHTML = "";
         document.querySelector("#showcase-servings").innerHTML = "";
     } else {
         showRecipe((array.filter(r => r.title != "firstRecipeDeletionMarker" && r.title != "secondRecipeDeletionMarker"))[0].title, false);
